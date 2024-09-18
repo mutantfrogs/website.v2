d1242bdb4afff43bac8ad37367e2657c6bdd6644
mutantfrogs <themutantfrogs@gmail.com>
2024-09-17 20:50:33 -0400

    added july/august 2024 art

--- src/components/AboutWindow.jsx
+++ src/components/AboutWindow.jsx
@@ -13,7 +13,7 @@ export default function AboutWindow() {
           <div className='flexContainerColumn'>
                 <article role="tabpanel" className='aboutArticle'>
                     <p style={{fontSize: '14px', textAlign:'center', marginBottom:'10px'}}>
-                        hello! my name is <b>jake/julia (they/them)</b> but i also go by <b>mutantfrogs</b> online. i'm a 23 year old nonbinary artist, programmer, and aspiring jack of all trades. i love minecraft, y2k aesthetics, rhythm games, old internet, and touching grass. this website is mostly a hub for my creations as i don't like posting to social media. expect frequent updates and additions!<br/>thank you for visiting and enjoy your stay!<br/>
+                        hello! my name is <b>jake (they/them)</b> but i also go by <b>mutantfrogs</b> online. i'm a 23 year old nonbinary artist, programmer, and aspiring jack of all trades. i love minecraft, y2k aesthetics, rhythm games, personal web, and touching grass. this website is mostly a hub for my creations as i don't like posting to social media. expect semi-frequent updates and additions!<br/>thank you for visiting and enjoy your stay!<br/>
                         <br/>
                         <b>discord: @mutantfrogs</b>
                     </p>                   

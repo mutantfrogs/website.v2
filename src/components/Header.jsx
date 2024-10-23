e0a8185d1e16e5a23a346d157f7042f7fce548a0
mutantfrogs <themutantfrogs@gmail.com>
2024-10-22 20:08:13 -0400

    added header easter egg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import defaultBanner from '../assets/defaultBanner.png';
+import {useState} from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
 
   const location = useLocation();
+  const [mystery, setMystery] = useState(false);
 
   const getBannerText = () => {
     switch (location.pathname) {
@@ -18,13 +20,21 @@ export default function Header() {
     }
   };
 
+  const mysteryClick = () => {
+    setMystery(!mystery);
+    if (!mystery)
+      document.querySelector('html').style.filter = 'invert(100%)';
+    else
+      document.querySelector('html').style.filter = '';
+  };
+
   return (
     <div className='flexContainerRow' style={{ marginTop: '15px' }}>
       <div className='window' style={{ width: 650 }}>
         <div className="title-bar">
           <div className="title-bar-text">🐸 welcome to my website!</div>
             <div className="title-bar-controls">
-              <button aria-label="Help"></button>
+              <button aria-label="Help" onClick={mysteryClick}></button>
               <button aria-label="Close"></button>
             </div>
         </div>
